f5a74c3eab4b38c831ad2fc9610bf31ebc5c0c0f
OkumuraRyn <166512321+OkumuraRyn@users.noreply.github.com>
2026-05-05 19:07:18 +0800

    Create a4.js

---
+++ src/data/articles/zsb/a4.js
@@ -0,0 +1,34 @@
+// src/data/articles/zsb/a4.js
+export default {
+  id: "ZSB-4",
+  type: "ZSB",
+  title: "The Behavioral Challenges of Gifted Children",
+  titleCn: "天才儿童的行为问题",
+  paragraphs: [
+    {
+      sentences: [
+        { en: "I read a letter of a mother of a gifted child to an expert with gifted children stating that her son was always complaining that he doesn't like his teacher for she talks in baby language and she lets them sing baby songs.", cn: "" },
+        { en: "Having an insufficient understanding of being different from their normally developing classmates, gifted children may find themselves wanting to learn more than being boxed with other kids of their age.", cn: "" },
+        { en: "They want to quench their thirst for knowledge.", cn: "" },
+        { en: "However, in the process, they tend to develop negative thoughts about school and teachers.", cn: "" },
+        { en: "Thus, they tend to exhibit behavioral problems.", cn: "" }
+      ]
+    },
+    {
+      sentences: [
+        { en: "Behavioral problems of gifted children could be mainly correlated（相关的）to their asynchronous（不同期的）development.", cn: "" },
+        { en: "This development means that the child has uneven development in their mind, body and emotions.", cn: "" },
+        { en: "To explain further, a gifted child may live in different ages at once.", cn: "" },
+        { en: "He may show high intellectual capabilities at some point but the time his emotions rule, he would go back to his true age going through tantrums（脾气发作）and endless sobbing.", cn: "" },
+        { en: "He may find it happy playing with kids of his age but the next day, he would be bored that he would find conversing with older kids to be more challenging.", cn: "" }
+      ]
+    },
+    {
+      sentences: [
+        { en: "Because of high intellectual level, a gifted child often acts differently in his own classroom.", cn: "" },
+        { en: "He has the thinking of being above from the rest of his classmates when it comes to grades, academic awards and school recognition.", cn: "" },
+        { en: "In classroom discussions, the child may either be very cooperative having advanced knowledge in the lessons but it is mostly that he will suffer from boredom as he is not challenged by the lessons and he would feel no improvement with them.", cn: "" }
+      ]
+    }
+  ]
+};
